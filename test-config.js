7dbd66ab1d7aa9f594da1a04c550ed38943e0e1f
Datpmt <chiducdat1@gmail.com>
2024-11-29 14:54:43 +0700

    update actions

--- test-config.js
+++ test-config.js
@@ -15,7 +15,7 @@ console.log(`PULL_NUMBER: ${pullNumber}`);
 console.log(`COMMIT_ID: ${commitId}`);
 
 async function sendPostRequest({githubToken, owner, repo, pullNumber, commitId, body, path, comment, position }) {
-  const url = `/repos/${owner}/${repo}/pulls/${pullNumber}/reviews`;
+  const url = `/repos/${repo}/pulls/${pullNumber}/reviews`;
 
   const data = {
     commit_id: commitId,
@@ -35,7 +35,7 @@ async function sendPostRequest({githubToken, owner, repo, pullNumber, commitId, 
 
   const axiosInstance = axios.create({
     baseURL: 'https://api.github.com',
-    headers: { Authorization: `token ${githubToken}` },
+    headers: { Authorization: `Bearer ${githubToken}` },
   });
 
   try {
