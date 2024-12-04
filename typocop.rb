7e211a41cbc773bcd11f48725327fb5bfaf75b26
Datpmt <chiducdat1@gmail.com>
2024-12-04 14:09:16 +0700

    test rb

--- typocop.rb
+++ typocop.rb
@@ -5,7 +5,7 @@ require 'base64'
 encoded_typo_outputs = ENV['ENCODED_TYPO_OUTPUTS'] || ''
 
 if encoded_typo_outputs.empty?
-  puts 'No typo ouput.'
+  puts 'No typo output.'
 else
   typos = Base64.decode64(encoded_typo_outputs).split("\n")
   typos.each do |typo|
