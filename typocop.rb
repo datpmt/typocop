6d84b0683f7aeac5c1ce3f27eab5a4dc47988479
Datpmt <chiducdat1@gmail.com>
2024-12-04 10:48:23 +0700

    test rb

--- typocop.rb
+++ typocop.rb
@@ -5,7 +5,7 @@ changed_file_list = ENV['CHANGED_FILE_LIST'] || ''
 if changed_file_list.empty?
   puts 'No files changed.'
 else
-  files = changed_file_list.split("\n")
+  files = changed_file_list.split
   files.each do |file|
     puts "Changed file: #{file}"
   end
