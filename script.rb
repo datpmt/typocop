bb9bb9ccfe9a99084e7c40144b001e4dbd454daa
Datpmt <chiducdat1@gmail.com>
2024-12-04 10:43:33 +0700

    test rb

--- script.rb
+++ script.rb
@@ -1,4 +1,12 @@
 # script.rb
 
-changed_file_list = ENV['CHANGED_FILE_LIST']
-puts "changed_file_list: #{changed_file_list}"
+changed_file_list = ENV['CHANGED_FILE_LIST'] || ''
+
+if changed_file_list.empty?
+  puts 'No files changed.'
+else
+  files = changed_file_list.split("\n")
+  files.each do |file|
+    puts "Changed file: #{file}"
+  end
+end
