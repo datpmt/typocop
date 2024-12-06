d40fb443853f4bf732aef9c9d8e019a79d97974a
Datpmt <chiducdat1@gmail.com>
2024-12-06 14:32:24 +0700

    test rb

--- test/example.rb
+++ test/example.rb
@@ -2,10 +2,6 @@ def greeting(name)
   puts "Hello, #{name}! Welocome to the Ruby typos test."
   puts 'languege' # typo
   puts 'knowlege' # typo
-  puts 'knowlege' # typo
-  puts 'knowlege: languege' # typo
-  puts 'knowlege: languege' # typo
-  puts 'knowlege: languege' # typo
   puts 'knowlege: languege' # typo
   puts 'welcom'
 end
