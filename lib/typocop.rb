5b22342c81282091d87d7c86d2268edd5fb1d698
Datpmt <chiducdat1@gmail.com>
2025-01-17 14:13:37 +0700

    Upgrade gem typo_checker v0.1.8

--- lib/typocop.rb
+++ lib/typocop.rb
@@ -11,23 +11,27 @@ require 'typocop/patch'
 require 'typocop/repo'
 require 'typo_checker'
 
-GITHUB_TOKEN = ENV['GITHUB_TOKEN'] || ''
-PULL_ID = ENV['PULL_REQUEST_ID']
-GITHUB_BASE_REF = ENV['GITHUB_BASE_REF'] || 'main'
+GITHUB_TOKEN = ENV.fetch('GITHUB_TOKEN') { raise 'GITHUB_TOKEN is required' }
+PULL_ID = ENV.fetch('PULL_REQUEST_ID') { raise 'PULL_REQUEST_ID is required' }
+GITHUB_BASE_REF = ENV.fetch('GITHUB_BASE_REF') { raise 'GITHUB_BASE_REF is required' }
 BASE_BRANCH = GITHUB_BASE_REF.start_with?('origin/') ? GITHUB_BASE_REF : "origin/#{GITHUB_BASE_REF}"
 
 module Typocop
   def self.execute(settings)
+    repo = Repo.new
+    paths = repo.patch_additions.map(&:path)
+
+    return unless paths.any?
+
     excludes = settings.excludes
     skips = settings.skips
-    typo_checker = TypoChecker::Checker.new(excludes, skips, stdoutput = false)
+    typo_checker = TypoChecker::Checker.new(paths: paths, excludes: excludes, skips: skips, stdoutput: false)
     found_typos = typo_checker.scan_repo('.')
 
     if found_typos.empty?
-      puts 'No typos.'
+      puts 'No typos found'
     else
       cops = Cops.new(found_typos)
-      repo = Repo.new
       client = Client.new(repo)
       client.execute(cops.cops)
     end
