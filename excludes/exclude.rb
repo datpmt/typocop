15e9e0d9a494a4ca3d3c86e64614f38c1207e323
Datpmt <chiducdat1@gmail.com>
2025-02-26 08:47:02 +0700

    Remove example files to clean up repository

--- excludes/exclude.rb
+++
@@ -1,9 +0,0 @@
-def greeting(name)
-  puts "Hello, #{name}! Welocome to the Ruby typos test."
-  puts 'languege' # typo
-  puts 'knowlege' # typo
-  puts 'knowlege: languege' # typo
-  puts 'welcom'
-end
-
-greeting('Alice')
